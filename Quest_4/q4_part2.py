da6dce6316fa37809d1d7e9291118630a47bd2c6
Aron Wallaker <aron.wallaker@gmail.com>
2025-11-11 15:45:50 -0500

    Q4 Part 2

--- Quest_4/q4_part2.py
+++ Quest_4/q4_part2.py
@@ -2,7 +2,7 @@ import math
 import sys
 from typing import List
 
-rot0 = 2025
+rot = 10000000000000
 
 def solution(lines:List[str]) -> int:
     gears = [int(x) for x in lines if x.strip != '']
@@ -14,10 +14,10 @@ def solution(lines:List[str]) -> int:
             ratio = ratio * (prev/g)
         prev = g
     print(f"Total ration: {ratio}")
-    return math.floor(rot0 * ratio)
+    return math.ceil(rot / ratio)
 
 if __name__ == '__main__':
-    print(f"*** Everybody Codes 2025, Quest 4, Part 1 ***\n")
+    print(f"*** Everybody Codes 2025, Quest 4, Part 2 ***\n")
     fname = sys.argv[1] if len(sys.argv) >=2 else 'sample1.txt'
     df = open(fname, "r")
     lines = df.read().splitlines()
